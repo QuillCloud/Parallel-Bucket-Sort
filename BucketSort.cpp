630e0549bfef725b8ec8eb5bdbd0dc31bf886f53
awszwc <xlb25@126.com>
2017-10-20 01:45:20 +1100

    a

--- BucketSort.cpp
+++ BucketSort.cpp
@@ -6,6 +6,9 @@
 #include <iostream>
 #include <thread>
 
+typedef std::pair<unsigned int, std::vector<unsigned int>> pair;
+typedef std::pair<const unsigned int, std::vector<unsigned int>> pair_c;
+
 bool aLessB(const unsigned int& x, const unsigned int& y, unsigned int pow) {
 
     if (x == y) return false; // if the two numbers are the same then one is not less than the other
@@ -51,18 +54,18 @@ void BucketSort::sort(unsigned int numCores) {
         }
         // create bucket if not exists, and put the number into bucket
         if ( buckets.find(first_digit) == buckets.end() ) {
-            buckets.insert(std::pair<unsigned int, std::vector<unsigned int>>(first_digit, std::vector<unsigned int>()));
+            buckets.insert(pair(first_digit, std::vector<unsigned int>()));
         }
         // put the number into bucket
         buckets.at(first_digit).push_back(num);
     }
     // lambda function for a single thread, sort each bucket for this thread
     auto sortFunc = [&buckets] (std::vector<unsigned int> bucket_list) {
-        std::for_each(bucket_list.begin(), bucket_list.end(), [&buckets] (unsigned int& i) {
+        for (auto& i : bucket_list) {
             std::sort(buckets[i].begin(), buckets[i].end(), [] (const unsigned int& x, const unsigned int& y){
                 return aLessB(x,y,0);
             } );
-        });
+        }
     };
     // store the location of buckets for each thread
     std::vector<std::vector<unsigned int>> l;
@@ -70,27 +73,27 @@ void BucketSort::sort(unsigned int numCores) {
     std::vector<unsigned long> l_size;
     // initialize the 'l'(start with 0 bucket for each thread) and 'l_size' (start with 0 for each thread)
     for (unsigned int i = 0; i < numCores; ++i) {
-        l.push_back(std::vector<unsigned int>());
+        l.emplace_back(std::vector<unsigned int>());
         l_size.push_back(0);
     }
     // store each thread
     std::vector<std::thread> containerOfThreads;
     // distribute buckets for each thread, each time add current bucket to the thread with smallest size
-    std::for_each(buckets.begin(), buckets.end(), [&l, &l_size] (std::pair<const unsigned int, std::vector<unsigned int>>& i) {
+    for (auto& i : buckets) {
         auto min_index = std::min_element(l_size.begin(),l_size.end()) - l_size.begin();
         l[min_index].push_back(i.first);
         l_size[min_index] += i.second.size();
-    });
+    }
     // start each thread
-    std::for_each(l.begin(), l.end(), [&containerOfThreads, &sortFunc] (std::vector<unsigned int>& i) {
-        containerOfThreads.push_back(std::thread(sortFunc, i));
-    });
+    for (auto& i : l) {
+        containerOfThreads.emplace_back(std::thread(sortFunc, i));
+    }
     // join each thread
-    std::for_each(containerOfThreads.begin(), containerOfThreads.end(), [] (std::thread& i) {
+    for (auto& i : containerOfThreads) {
         i.join();
-    });
+    }
     // combine the sorted buckets
-    std::for_each(buckets.begin(), buckets.end(), [&] (std::pair<const unsigned int, std::vector<unsigned int>> &i) {
+    for (auto& i : buckets) {
         numbersToSort.insert(numbersToSort.end(), i.second.begin(), i.second.end());
-    });
+    }
 }
